51f3417407c9448bd04c5bbef4802720ccbc38a9
Darshan Karagir <karagirdarshan1@gmail.com>
2026-02-23 20:59:33 +0530

    Deploying

--- src/main/java/com/example/urbanRides/Config/WebConfig.java
+++ src/main/java/com/example/urbanRides/Config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173",
+                                "https://urban-rides-website.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
